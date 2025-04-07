facd28612d8c99298847629d6b226a809eb7e380
Egor Gorbachev <egor.v.gor@gmail.com>
2025-04-07 16:08:49 +0300

    first commit

---
+++ lock-freeList.go
@@ -0,0 +1,40 @@
+package LockFree
+
+import (
+	"fmt"
+	"sync/atomic"
+	"unsafe"
+)
+
+type List struct {
+	head unsafe.Pointer
+}
+
+type Node struct {
+	value int
+	next unsafe.Pointer
+}
+
+func (l *List) Add(value int) {
+	node := &Node{value: value}
+	for {
+		oldHead := atomic.LoadPointer(&l.head)
+		node.next = oldHead
+
+		if atomic.CompareAndSwapPointer(&l.head, oldHead, unsafe.Pointer(node)) {
+			break
+		}
+	}
+}
+
+func (l *List) Print() {
+	curr := atomic.LoadPointer(&l.head)
+
+	for curr != nil {
+		node := (*Node)(curr)
+		fmt.Println(node.value)
+		curr = atomic.LoadPointer((*unsafe.Pointer)(node.next))
+	}
+}
+
+// TODO: Make finding by index (FindByIndex func)
